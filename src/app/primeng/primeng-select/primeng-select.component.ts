de78d85775743a53e4dbd8f6acdab69cc760a37a
shirley543 <shirleyx38@hotmail.com>
2025-02-11 15:20:18 +1100

    Primeng: multiselect

--- src/app/primeng/primeng-select/primeng-select.component.ts
+++ src/app/primeng/primeng-select/primeng-select.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SelectItemGroup } from 'primeng/api';
 import { Select } from 'primeng/select';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 interface City {
   name: string;
@@ -9,15 +11,19 @@ interface City {
 
 @Component({
   selector: 'app-primeng-select',
-  imports: [FormsModule, Select],
+  imports: [FormsModule, Select, MultiSelectModule],
   templateUrl: './primeng-select.component.html',
   styleUrl: './primeng-select.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PrimengSelectComponent { 
+export class PrimengSelectComponent {
+  // For single select
   cities: City[] | undefined;
-
   selectedCity: City | undefined;
+
+  // For multi select + grouped
+  groupedCities!: SelectItemGroup[];
+  selectedCities!: City[];
 
   ngOnInit() {
     this.cities = [
@@ -26,6 +32,39 @@ export class PrimengSelectComponent {
       { name: 'London', code: 'LDN' },
       { name: 'Istanbul', code: 'IST' },
       { name: 'Paris', code: 'PRS' }
+    ];
+
+    this.groupedCities = [
+      {
+        label: 'Germany',
+        value: 'de',
+        items: [
+          { label: 'Berlin', value: 'Berlin' },
+          { label: 'Frankfurt', value: 'Frankfurt' },
+          { label: 'Hamburg', value: 'Hamburg' },
+          { label: 'Munich', value: 'Munich' }
+        ]
+      },
+      {
+        label: 'USA',
+        value: 'us',
+        items: [
+          { label: 'Chicago', value: 'Chicago' },
+          { label: 'Los Angeles', value: 'Los Angeles' },
+          { label: 'New York', value: 'New York' },
+          { label: 'San Francisco', value: 'San Francisco' }
+        ]
+      },
+      {
+        label: 'Japan',
+        value: 'jp',
+        items: [
+          { label: 'Kyoto', value: 'Kyoto' },
+          { label: 'Osaka', value: 'Osaka' },
+          { label: 'Tokyo', value: 'Tokyo' },
+          { label: 'Yokohama', value: 'Yokohama' }
+        ]
+      }
     ];
   }
 }
