9eb034bdac8835a9450fbb3fd7361cc3ed0ad35b
shirley543 <shirleyx38@hotmail.com>
2025-02-17 15:18:06 +1100

    Material table: Customizing to use PrimeNG customer data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { ButtonCollectionComponent } from "./collections/button-collection/butto
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { SelectCollectionComponent } from "./collections/select-collection/select-collection.component";
+import { PrimengTableComponent } from "./primeng/primeng-table/primeng-table.component";
+import { MaterialTable } from "./material/material-table/material-table.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonCollectionComponent, SelectCollectionComponent],
+  imports: [RouterOutlet, ButtonCollectionComponent, SelectCollectionComponent, PrimengTableComponent, MaterialTable],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/material/material-table/material-table.component.ts
+++ src/app/material/material-table/material-table.component.ts
@@ -9,46 +9,9 @@ import {MatSort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
+import { CustomerService } from '../../services/customer.service';
+import { Customer, Representative } from '../../domain/customer';
+import { CommonModule } from '@angular/common';
 
 /**
  * @title Data table with sorting, pagination, and filtering.
@@ -57,26 +20,63 @@ const NAMES: string[] = [
   selector: 'app-material-table',
   styleUrl: 'material-table.component.scss',
   templateUrl: 'material-table.component.html',
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
 })
 export class MaterialTable implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource: MatTableDataSource<UserData>;
+  // displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  displayedColumns: string[] = ['name', 'agent', 'date', 'balance', 'status', 'activity'];
+  // dataSource!: MatTableDataSource<UserData>;
+  dataSource!: MatTableDataSource<Customer>;
 
+  customers!: Customer[];
+  selectedCustomers!: Customer[];
+  representatives!: Representative[];
+  statuses!: any[];
+  
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+  constructor(private customerService: CustomerService) {
+  }
 
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+  ngOnInit() {
+    this.representatives = [
+      { name: 'Amy Elsner', image: 'amyelsner.png' },
+      { name: 'Anna Fali', image: 'annafali.png' },
+      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+      { name: 'Onyama Limba', image: 'onyamalimba.png' },
+      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+      { name: 'Xuxue Feng', image: 'xuxuefeng.png' }
+    ];
+
+    this.statuses = [
+      { label: 'Unqualified', value: 'unqualified' },
+      { label: 'Qualified', value: 'qualified' },
+      { label: 'New', value: 'new' },
+      { label: 'Negotiation', value: 'negotiation' },
+      { label: 'Renewal', value: 'renewal' },
+      { label: 'Proposal', value: 'proposal' }
+    ];
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.customerService.getCustomersLarge().then((customers) => {
+      this.customers = customers;
+      // this.loading = false;
+
+      this.customers.forEach((customer) => (customer.date = new Date(<Date>customer.date)));
+    
+      this.dataSource = new MatTableDataSource(this.customers);
+
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
   }
 
   applyFilter(event: Event) {
@@ -87,20 +87,4 @@ export class MaterialTable implements AfterViewInit {
       this.dataSource.paginator.firstPage();
     }
   }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
 }
